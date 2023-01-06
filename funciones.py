1176de6a291a8716f4e78a3c989db547e4980714
Hector Manuel Romo Lozano <hromolozano.95@gmail.com>
2023-01-05 21:10:04 -0600

    Funciones leer_url y crear_tabla

---
+++ funciones.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = "https://www.mediotiempo.com/futbol/liga-mx/tabla-general"
+
+
+def leer_url(url):
+    """
+    Manda request a la página de MedioTiempo y con bs4 transforma la respuesta
+    a un html
+
+    Argumentos:
+        Url de la página web
+    
+    Retorna:
+        Un html response en la variable soup
+    """
+    response = requests.get(url)
+
+    response.encoding = 'utf-8'
+
+    html = response.text
+
+    soup = BeautifulSoup(html,'html.parser')
+
+    return soup
+
+def crear_tabla(soup):    
+    all_data = soup.find_all('tr', {'class': 'ctr-stadistics-header__tr'})
+    list = []
+    for i in range(1,len(all_data)):
+        list.append(all_data[i].text)
+
+    lista_nueva = [ x.split('\n') for x in list]
+    df2 = pd.DataFrame(lista_nueva)
+    df3 = df2.drop([0,2,3,5,6,15],axis=1)
+    df3 = df3.rename(columns={1:"Posición",4:"Equipo",7:"Juegos jugados",8:"Juegos ganados",
+                9:"Juegos empatados",10:"Juegos perdidos",
+                11:"Goles a favor",12:"Juegos en contra", 13:"Diferencia de goles", 
+                14:"Puntos"})
